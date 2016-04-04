0807a77581cc444424f48f3ed481917bc6fa7666
Rahul khanna <rahul.khanna@nyu.edu>
2016-04-04 16:26:15 -0400

    New Ride sharing request bean

---
+++ RideShare/src/main/java/edu/nyu/cloud/service/beans/IncomingPoolRequest.java
@@ -0,0 +1,54 @@
+package edu.nyu.cloud.service.beans;
+
+import java.util.Date;
+
+/**
+ * This class represents the new car pool request which is been opened by the User to share the ride.  
+ * 
+ * @author rahulkhanna Date:04-Apr-2016
+ */
+public class IncomingPoolRequest {
+
+	private final int numberOfPassengers;
+	private final String destination;
+	private final String source;
+	private final Date date;
+	private final String userId;
+
+	/**
+	 * @param numberOfPassengers
+	 * @param destination
+	 * @param source
+	 * @param date
+	 * @param userId
+	 */
+	public IncomingPoolRequest(int numberOfPassengers, String destination, String source, Date date, String userId) {
+		super();
+		this.numberOfPassengers = numberOfPassengers;
+		this.destination = destination;
+		this.source = source;
+		this.date = date;
+		this.userId = userId;
+	}
+
+	public int getNumberOfPassengers() {
+		return numberOfPassengers;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+}
