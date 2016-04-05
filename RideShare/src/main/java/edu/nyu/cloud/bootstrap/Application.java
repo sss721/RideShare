c2e7ccde53dba546e119830b5271f97ade427e0c
Rahul khanna <rahul.khanna@nyu.edu>
2016-04-05 14:59:27 -0400

    Added comments

--- RideShare/src/main/java/edu/nyu/cloud/bootstrap/Application.java
+++ RideShare/src/main/java/edu/nyu/cloud/bootstrap/Application.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * This class is used to bootstrap the application.
+ * 
+ * @author rahulkhanna
+ * Date:05-Apr-2016
+ */
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "edu.nyu.cloud")
 public class Application {
