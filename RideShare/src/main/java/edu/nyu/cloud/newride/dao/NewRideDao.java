e354f7f9d8aa75d4ef0b1cacc80f4c585910139d
Rahul khanna <rahul.khanna@nyu.edu>
2016-04-06 18:40:00 -0400

    Classes for persisting new ride data.

---
+++ RideShare/src/main/java/edu/nyu/cloud/newride/dao/NewRideDao.java
@@ -0,0 +1,11 @@
+package edu.nyu.cloud.newride.dao;
+
+import edu.nyu.cloud.beans.NewRide;
+import edu.nyu.cloud.dao.db.BaseDatabaseDao;
+import edu.nyu.cloud.persistent.bean.PersistentNewRide;
+
+public interface NewRideDao extends BaseDatabaseDao<NewRide, PersistentNewRide> {
+
+	
+	
+}
---
+++ RideShare/src/main/java/edu/nyu/cloud/persistent/bean/PersistentNewRide.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.nyu.cloud.persistent.bean;
+
+/**
+ * @author rahulkhanna
+ * Date:05-Apr-2016
+ */
+public class PersistentNewRide {
+
+}
